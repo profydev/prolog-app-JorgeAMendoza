794b4e71829e4bcece3770c08d7365d707f6c69b
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-10-02 12:07:33 -0500

    create loading component story book
    create loading component with story so it can be modified in storybook.
    Three versions of the loading component created in three different sizes
    (sm, md, lg).

---
+++ features/ui/loading/Loading.tsx
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import style from "./loading.module.scss";
+
+export enum LoadingSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+}
+
+type LoadingProps = {
+  size?: LoadingSize;
+};
+
+export const Loading = ({ size = LoadingSize.md }: LoadingProps) => {
+  return <div className={classNames(style.loading, style[size])}></div>;
+};
